3ddbe87a25532c35a98c02228085a7d36610a303
Drucial <drucial@Mac.attlocal.net>
2025-10-17 23:52:44 -0400

    wip on initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/ui/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
@@ -50,6 +51,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+        <ModeToggle />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
